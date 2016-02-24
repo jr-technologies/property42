ef9a0723b9dfb2c5f5959837234adffddfe17c7c
noman <jrproperty167@gmail.com>
2016-02-24 17:16:35 +0500

    migrations uploaded by noman

--- database/migrations/58_CreatePropertyDocumentsTable.php
+++ database/migrations/58_CreatePropertyDocumentsTable.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePropertyDocumentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('property_documents', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->integer('property_id')->unsigned();
+            $table->string('type');
+            $table->text('path');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('property_documents');
     }
 }
