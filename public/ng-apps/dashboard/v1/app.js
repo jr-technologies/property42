f9c421c61c6ba3c9a1f987b7287bbcf2a1802474
nomantufail <nomantufail100@gmail.com>
2016-06-27 20:18:19 +0500

    actives are now actives

--- public/ng-apps/dashboard/v1/app.js
+++ public/ng-apps/dashboard/v1/app.js
@@ -70,6 +70,11 @@ app.run(function($rootScope, $location, $AuthService, $state, $ErrorResponseHand
 
     $rootScope.propertiesCounts = {};
     $rootScope.$on( "$stateChangeStart", function(event, next, current) {
+        console.log($AuthService.getAppToken());
+        if($rootScope.authUser != null && $AuthService.getAppToken() == null){
+            alert('session expired! please login again.');
+            window.location.href = domain+'logout';
+        }
         $rootScope.activeLink = next.name;
         $rootScope.loading_content_class = 'loading-content';
         /*
