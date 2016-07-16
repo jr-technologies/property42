47ba990fe4c20ea80a2236c14eb8299270421b73
nomantufail <nomantufail100@gmail.com>
2016-07-16 09:14:57 -0700

    minor changes

--- public/ng-apps/dashboard/v1/routes.js
+++ public/ng-apps/dashboard/v1/routes.js
@@ -1,10 +1,10 @@
 /**
  * Created by noman_2 on 12/8/2015.
  */
-var domain = "http://"+window.location.hostname+"/p42/public/";
+//var domain = "http://"+window.location.hostname+"/p42/public/";
 //var domain = "http://"+window.location.hostname+"/jr/property42/backend/property42/public/";
 //var domain = "http://"+window.location.hostname+"/production/jr-technologies/property42/public/";
-//var domain = "http://"+window.location.hostname+"/property42/public/";
+var domain = "http://"+window.location.hostname+"/property42/public/";
 
 var api = "api/v1/";
 var apiPath = domain+api;
