8324b1be924043904ce25d42aeb37ece2177aad4
nomantufail <nomantufail100@gmail.com>
2016-08-03 22:30:49 +0500

    comited

--- app/Http/Controllers/Web/AppsController.php
+++ app/Http/Controllers/Web/AppsController.php
@@ -10,9 +10,36 @@ use App\Http\Responses\Responses\WebResponse;
 
 class AppsController extends Controller
 {
+    public $purposes  = "";
+    public $statuses  = "";
+    public $societies = "";
+    public $propertyTypes = "";
+    public $propertySubTypes = "";
+    public $landUnits   = "";
+    public $agencyStaff = "";
+    public $response = null;
+    public $userAgency ="";
+    public $properties ="";
+    public $assignedFeaturesJson="";
+    public $propertyStatuses = null;
+    public $propertyJsonFactory = null;
+    public $user = null;
+
     public function __construct(WebResponse $webResponse)
     {
         $this->response = $webResponse;
+        $this->purposes = (new PropertyPurposesRepoProvider())->repo();
+        $this->statuses = (new PropertyStatusesRepoProvider())->repo();
+        $this->societies = (new SocietiesRepoProvider())->repo();
+        $this->propertyTypes = (new PropertyTypesRepoProvider())->repo();
+        $this->propertySubTypes = (new PropertySubTypesRepoProvider())->repo();
+        $this->landUnits = (new LandUnitsRepoProvider())->repo();
+        $this->userAgency = (new AgenciesRepoProvider())->repo();
+        $this->agencyStaff = (new UsersJsonRepoProvider())->repo();
+        $this->properties = (new PropertiesRepoProvider())->repo();
+        $this->assignedFeaturesJson = (new AssignedFeatureJsonRepoProvider())->repo();
+        $this->propertyStatuses = (new PropertyStatusesRepoProvider())->repo();
+        $this->propertyJsonFactory = new PropertyJsonFactory();
     }
 
     public function dashboard(GetDashboardAppRequest $appRequest)
